3f10159d362527eb0078447fd4c5363dd7c1be38
Francisco <fcodepablo@gmail.com>
2026-03-08 21:30:18 -0300

    build: better dependencies specification

--- tests/Pest.php
+++ tests/Pest.php
@@ -28,20 +28,3 @@ pest()->extend(Panchodp\LaravelAction\Test\TestCase::class)
 |
 */
 
-expect()->extend('toBeOne', fn () => $this->toBe(1));
-
-/*
-|--------------------------------------------------------------------------
-| Functions
-|--------------------------------------------------------------------------
-|
-| While Pest is very powerful out-of-the-box, you may have some testing code specific to your
-| project that you don't want to repeat in every file. Here you can also expose helpers as
-| global functions to help you to reduce the number of lines of code in your test files.
-|
-*/
-
-function something(): void
-{
-    // ..
-}
