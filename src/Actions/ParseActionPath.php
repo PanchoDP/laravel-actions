2bc0c5b58339978621078487fdd29099532e80e3
Francisco <fcodepablo@gmail.com>
2026-05-18 14:36:32 -0400

    fix: phpstan type error

--- src/Actions/ParseActionPath.php
+++ src/Actions/ParseActionPath.php
@@ -17,7 +17,7 @@ final class ParseActionPath
      */
     public static function handle(string $name, string $subfolder): array
     {
-        $subfolder = mb_trim($subfolder, '/\\');
+        $subfolder = (string) mb_trim($subfolder, '/\\');
 
         if (! preg_match('#[/\\\\]#', $name)) {
             return ['name' => $name, 'subfolder' => $subfolder];
